179c03eb7212715c7411175cf1f732a9bc9a67c9
Afonso Rocha <afonsoandrerocha@gmail.com>
2019-06-15 16:15:24 -0300

    Ruby Workshop third day

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,15 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  post 'artists/import', to: 'imports#artist'
+
+  
+
   resources :artists do
-    resources :releases
+    resources :releases do
+      collection do
+        post :import, to: 'imports#artist_releases'
+      end
+    end
   end
 end
